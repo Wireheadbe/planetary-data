ad830df0938e5bc7c2e239eccdd9b80be70342dc
Jeffrey Vandenbroucke <jeffrey@wirehead.be>
2025-07-29 21:04:16 +0200

    fix async

--- custom_components/planetary-data/__init__.py
+++ custom_components/planetary-data/__init__.py
@@ -1,9 +1,10 @@
 DOMAIN = "planetary_data"
 
 async def async_setup_entry(hass, entry):
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(config_entry, "sensor")
+    #hass.async_create_task(
+    #    hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    #)
     return True
 
 async def async_unload_entry(hass, entry):
