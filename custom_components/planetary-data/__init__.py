9591f72dcb80adcfaf6d97210dfd709cada2e2ed
Jeffrey Vandenbroucke <jeffrey@wirehead.be>
2025-07-29 21:11:36 +0200

    cleanup

--- custom_components/planetary-data/__init__.py
+++ custom_components/planetary-data/__init__.py
@@ -2,9 +2,6 @@ DOMAIN = "planetary_data"
 
 async def async_setup_entry(hass, entry):
     await hass.config_entries.async_forward_entry_setups(entry, "sensor")
-    #hass.async_create_task(
-    #    hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    #)
     return True
 
 async def async_unload_entry(hass, entry):
